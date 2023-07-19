382e0b2f4aab887be2921ecb57e879191aff46b5
thaivd2 <thaivd2@tpb.com.vn>
2023-07-19 19:10:53 +0700

    add secrets

--- week_6_stream_processing/java/kafka_examples/src/main/java/org/example/Secrets.java
+++ week_6_stream_processing/java/kafka_examples/src/main/java/org/example/Secrets.java
@@ -1,10 +1,9 @@
 package org.example;
 
 public class Secrets {
-    public static final String KAFKA_CLUSTER_KEY = "REPLACE_WITH_YOUR_KAFKA_CLUSTER_KEY";
-    public static final String KAFKA_CLUSTER_SECRET = "REPLACE_WITH_YOUR_KAFKA_CLUSTER_SECRET";
+    public static final String KAFKA_CLUSTER_KEY = "7GPZS4K5D33XDCVQ";
+    public static final String KAFKA_CLUSTER_SECRET = "L5iGyWEqtTwbgpSJoKefUC5ZwvyCWZwR4AUyoTL/u8yr9dKGDG+31hIfOORzuxHz";
 
-    public static final String SCHEMA_REGISTRY_KEY = "REPLACE_WITH_SCHEMA_REGISTRY_KEY";
-    public static final String SCHEMA_REGISTRY_SECRET = "REPLACE_WITH_SCHEMA_REGISTRY_SECRET";
-
+    public static final String SCHEMA_REGISTRY_KEY = "X7BPCPTYE24E7V7S";
+    public static final String SCHEMA_REGISTRY_SECRET ="DsVLVa6907dg6cAchNCES2kKhoe8B61qY5J6u05ltF93/69E7mbOsroY0+nxaVl5";
 }
